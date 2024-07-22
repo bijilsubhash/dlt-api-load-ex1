f3a8aad9984972cfcdf39d2973166bfe57cd369e
Bijil Subhash <bijil.subhash@mantelgroup.com.au>
2024-07-22 16:34:41 +1000

    first commit

---
+++ adzuna.py
@@ -0,0 +1,40 @@
+import dlt
+from dotenv import load_dotenv
+import os
+import requests
+
+@dlt.resource(
+        table_name="adzuna", 
+        write_disposition="merge", 
+        primary_key='id', 
+        columns={"location": {"data_type": "complex"}}
+        )
+def get_jobs(APP_ID, APP_KEY):
+    page_number = 1
+    while True:
+        params = {
+            'app_id': APP_ID, 
+            'app_key': APP_KEY, 
+            'results_per_page': 1000, 
+            'what': 'data engineer', 
+            'max_days_old': 7
+            }
+        url = f"https://api.adzuna.com/v1/api/jobs/au/search/{page_number}"
+        response = requests.get(url, params)
+        response.raise_for_status()
+        if response.json()['results']:
+            yield response.json()['results']
+            page_number += 1
+            continue
+        break
+
+if __name__ == "__main__":
+    load_dotenv()
+    pipeline = dlt.pipeline(
+        pipeline_name="adzuna",
+        destination="bigquery",
+        dataset_name="jobs"
+    )
+
+    load_info = pipeline.run(get_jobs(os.getenv('APP_ID'), os.getenv('APP_KEY')))
+    print(load_info)
